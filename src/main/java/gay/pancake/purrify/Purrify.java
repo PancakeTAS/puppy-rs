490d36c063ead03122214a326a91b88263eceeba
PancakeTAS <pancake@mgnet.work>
2023-12-14 20:04:07 +0100

    Attempt to fix pings

--- src/main/java/gay/pancake/purrify/Purrify.java
+++ src/main/java/gay/pancake/purrify/Purrify.java
@@ -47,6 +47,7 @@ public class Purrify extends ListenerAdapter {
         var token = System.getenv("PURRIFY_TOKEN");
         this.jda = JDABuilder.createDefault(token)
                 .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .enableIntents(GatewayIntent.GUILD_MEMBERS)
                 .addEventListeners(this)
                 .build().awaitReady();
 
@@ -77,85 +78,81 @@ public class Purrify extends ListenerAdapter {
      */
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
-        // Defer reply because we need to download the image
-        event.deferReply().queue(defer -> {
+        // Get endpoint
+        var category = event.getFullCommandName();
+        var endpoint = ApiEndpoints.getByName(category);
 
-            // Get endpoint
-            var category = event.getFullCommandName();
-            var endpoint = ApiEndpoints.getByName(category);
+        // Parse response
+        if (endpoint.getType() == ApiEndpoints.Type.IMAGE) {
+            var amountOption = event.getOption("amount");
+            var amount = amountOption == null ? 1 : Math.min(10, amountOption.getAsInt());
 
-            // Parse response
-            if (endpoint.getType() == ApiEndpoints.Type.IMAGE) {
-                var amountOption = event.getOption("amount");
-                var amount = amountOption == null ? 1 : Math.min(10, amountOption.getAsInt());
+            // Request images
+            this.api.getImage(category, amount).thenAccept(images -> {
 
-                // Request images
-                this.api.getImage(category, amount).thenAccept(images -> {
+                // Combine futures
+                var futures = images.stream().map(image -> this.api.get(image.url)).toList();
+                CompletableFuture.allOf(futures.toArray(CompletableFuture[]::new)).thenAccept(v -> {
 
-                    // Combine futures
-                    var futures = images.stream().map(image -> this.api.get(image.url)).toList();
-                    CompletableFuture.allOf(futures.toArray(CompletableFuture[]::new)).thenAccept(v -> {
+                    // Create reply message
+                    var artists = new StringBuilder();
+                    var sources = new StringBuilder();
+                    var files = new FileUpload[images.size()];
+                    for (var i = 0; i < images.size(); i++) {
+                        artists.append(images.get(i).artist_name).append(": <").append(images.get(i).artist_href).append(">\n");
+                        sources.append("<").append(images.get(i).source_url).append(">").append("\n");
+                        files[i] = FileUpload.fromData(futures.get(i).join(), "image" + i + "." + endpoint.getType().getExtension());
+                    }
 
-                        // Create reply message
-                        var artists = new StringBuilder();
-                        var sources = new StringBuilder();
-                        var files = new FileUpload[images.size()];
-                        for (var i = 0; i < images.size(); i++) {
-                            artists.append(images.get(i).artist_name).append(": <").append(images.get(i).artist_href).append(">\n");
-                            sources.append("<").append(images.get(i).source_url).append(">").append("\n");
-                            files[i] = FileUpload.fromData(futures.get(i).join(), "image" + i + "." + endpoint.getType().getExtension());
-                        }
+                    // Reply with images
+                    event.reply(endpoint.getMessage().replace("%SENDER%", event.getUser().getAsMention()) + "\n\nArtists:\n" + artists + "\nSources:\n" + sources)
+                            .addFiles(files).queue(msg -> {
+                                // Close files
+                                for (var file: files) {
+                                    try {
+                                        file.close();
+                                    } catch (IOException e) {
+                                        LOGGER.warn("Failed to close file", e);
+                                    }
+                                }
+                            });
+                });
 
-                        // Reply with images
-                        defer.sendMessage(endpoint.getMessage().replace("%SENDER%", event.getUser().getAsMention()) + "\n\nArtists:\n" + artists + "\nSources:\n" + sources)
-                                .addFiles(files).queue(msg -> {
-                                    // Close files
-                                    for (var file: files) {
-                                        try {
-                                            file.close();
-                                        } catch (IOException e) {
-                                            LOGGER.warn("Failed to close file", e);
-                                        }
+            });
+        } else {
+            var userOption = event.getOption("user");
+            var user = userOption == null ? null : userOption.getAsUser();
+
+            // Request gif
+            this.api.getGif(category, 1).thenAccept(gifs -> {
+                var gif = gifs.getFirst();
+                // Get gif
+                this.api.get(gifs.getFirst().url).thenAccept(image -> {
+                    // Reply with gif
+                    var file = FileUpload.fromData(image, "image." + endpoint.getType().getExtension());
+                    if (user == null) {
+                        event.reply(endpoint.getMessage().replace("%SENDER%", event.getUser().getAsMention()) + "\n\n" + "Anime: " + gif.anime_name)
+                                .addFiles(file).queue(msg -> {
+                                    // Close file
+                                    try {
+                                        file.close();
+                                    } catch (IOException e) {
+                                        LOGGER.warn("Failed to close file", e);
                                     }
                                 });
-                    });
-
+                    } else {
+                        event.reply(endpoint.getWithUserMessage().replace("%SENDER%", event.getUser().getAsMention()).replace("%TARGET%", user.getAsMention()) + "\n\n" + "Anime: " + gif.anime_name)
+                                .addFiles(file).queue(msg -> {
+                                    // Close file
+                                    try {
+                                        file.close();
+                                    } catch (IOException e) {
+                                        LOGGER.warn("Failed to close file", e);
+                                    }
+                                });
+                    }
                 });
-            } else {
-                var userOption = event.getOption("user");
-                var user = userOption == null ? null : userOption.getAsUser();
-
-                // Request gif
-                this.api.getGif(category, 1).thenAccept(gifs -> {
-                    var gif = gifs.getFirst();
-                    // Get gif
-                    this.api.get(gifs.getFirst().url).thenAccept(image -> {
-                        // Reply with gif
-                        var file = FileUpload.fromData(image, "image." + endpoint.getType().getExtension());
-                        if (user == null) {
-                            defer.sendMessage(endpoint.getMessage().replace("%SENDER%", event.getUser().getAsMention()) + "\n\n" + "Anime: " + gif.anime_name)
-                                    .addFiles(file).queue(msg -> {
-                                        // Close file
-                                        try {
-                                            file.close();
-                                        } catch (IOException e) {
-                                            LOGGER.warn("Failed to close file", e);
-                                        }
-                                    });
-                        } else {
-                            defer.sendMessage(endpoint.getWithUserMessage().replace("%SENDER%", event.getUser().getAsMention()).replace("%TARGET%", user.getAsMention()) + "\n\n" + "Anime: " + gif.anime_name)
-                                    .addFiles(file).queue(msg -> {
-                                        // Close file
-                                        try {
-                                            file.close();
-                                        } catch (IOException e) {
-                                            LOGGER.warn("Failed to close file", e);
-                                        }
-                                    });
-                        }
-                    });
-                });
-            }
-        });
+            });
+        }
     }
 }

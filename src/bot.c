7ed536baa2f5f9f3f5cc0634dacba80f53cda904
PancakeTAS <pancake@mgnet.work>
2024-03-26 21:55:21 +0100

    feat: periodically update bot status

--- src/bot.c
+++ src/bot.c
@@ -2,15 +2,8 @@
 
 endpoint_list all_endpoints;
 
-/**
- * Update bot status
- *
- * \param client Discord client
- * \param event Ready event
- *
- * \return 0 on success, 1 on failure
- */
-int update_status(struct discord *client, const struct discord_ready *event) {
+void do_update_status(struct discord *client, struct discord_response *response, const struct discord_guilds *guilds) {
+    // update status
     struct discord_presence_update presence = {
         .activities = &(struct discord_activities) {
             .size = 1,
@@ -24,11 +17,21 @@ int update_status(struct discord *client, const struct discord_ready *event) {
         .afk = false,
         .since = discord_timestamp(client)
     };
-    snprintf(presence.activities->array->state, 48, "Serving %d guilds with %d endpoints.", event->guilds->size, all_endpoints.len);
+    snprintf(presence.activities->array->state, 48, "Serving %d guilds with %d endpoints.", guilds->size, all_endpoints.len);
     discord_set_presence(client, &presence);
     free(presence.activities->array->state);
+}
 
-    return 0;
+/**
+ * Update bot status
+ *
+ * \param client Discord client
+ * \param event Ready event
+ */
+void update_status(struct discord *client, struct discord_timer *event) {
+    // get guild count
+    struct discord_ret_guilds ret = { .done = do_update_status };
+    discord_get_current_user_guilds(client, &ret);
 }
 
 void init(struct discord *client, const struct discord_ready *event) {
@@ -61,14 +64,10 @@ void init(struct discord *client, const struct discord_ready *event) {
     log_info("[BOT] Initialized slash commands");
 
     // update status
-    log_info("[BOT] Updating status...");
-    if (update_status(client, event)) {
-        log_fatal("[BOT] Failed to update status");
+    log_info("[BOT] Initializing status update timer...");
+    discord_timer_interval(client, update_status, NULL, NULL, 0, 60 * 60 * 1000, -1);
+    log_info("[MAIN] Initialized status update timer");
 
-        discord_shutdown(client);
-        return;
-    }
-    log_info("[MAIN] Initialized status");
 }
 
 void deinit(void) {

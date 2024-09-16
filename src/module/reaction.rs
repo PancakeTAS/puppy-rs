1eb4646eaf0c1a9bd29988d0fc8ec7ff6603a248
PancakeTAS <pancake@mgnet.work>
2024-09-16 12:03:35 +0200

    feat: note for adding bot to user

--- src/module/reaction.rs
+++ src/module/reaction.rs
@@ -167,9 +167,16 @@ impl Module for ReactionModule {
                 .context("no default response")?
         };
 
-        let message = message.replace("{user}", format!("<@{}>", user.get()).as_str())
+        let mut message = message.replace("{user}", format!("<@{}>", user.get()).as_str())
             .replace("{target}", format!("<@{}>", target.get()).as_str())
             + format!("\n-# From: {} • [Source](<{}>)", backend, image_url).as_str();
+
+        if !cmd.data.name.starts_with("reaction") {
+            message += format!(
+                " • [Add me to your user/guild!](<https://discord.com/oauth2/authorize?client_id={}>)",
+                cmd.application_id.get()
+            ).as_str();
+        }
 
         let color = crate::color::rand();
 
